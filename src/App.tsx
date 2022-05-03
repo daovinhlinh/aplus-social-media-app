5755ebc8e74a9e9ec72abca1b13362640e1f273c
Linh <linh142000@gmail.com>
2022-05-03 09:43:15 +0700

    Add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css';
-import NewFeed from './pages/NewFeed';
+import "./App.css";
+import Login from "./pages/Login";
+import NewFeed from "./pages/NewFeed";
 
 function App() {
-  return (
-    <div className="App">
-      <NewFeed />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Login />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/assets/Icons/Send.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Send = () => {
+    return (
+        <svg
+            width="26"
+            height="24"
+            viewBox="0 0 26 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g clip-path="url(#clip0_6_179)">
+                <path
+                    d="M10.2806 11.9999H3.35979L1.04542 3.2609C1.0306 3.21012 1.02158 3.15796 1.0185 3.10535C0.992744 2.30423 1.92224 1.74867 2.72764 2.11534L24.4314 11.9999L2.72764 21.8844C1.9316 22.2477 1.01382 21.7077 1.0185 20.921C1.02087 20.8507 1.03388 20.7811 1.05713 20.7144L2.77446 15.3332"
+                    stroke="#9D9D9D"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </g>
+            <defs>
+                <clipPath id="clip0_6_179">
+                    <rect width="25.0131" height="24" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
+
+export default Send;
