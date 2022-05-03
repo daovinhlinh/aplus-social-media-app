857ba81e682971f7491bb30c9fd8ccf563ea1de9
Linh <linh142000@gmail.com>
2022-05-03 09:45:34 +0700

    Add login screen

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,53 @@
+import { Box, color, Flex, Input, Text } from "@chakra-ui/react";
+import React from "react";
+import Emoji from "../../assets/Icons/Emoji";
+import People from "../../assets/Icons/People";
+import Photo from "../../assets/Icons/Photo";
+import Send from "../../assets/Icons/Send";
+import CustomButton from "../../components/CustomButton";
+import InputField, { Variant } from "../../components/InputField";
+import styles from "./styles.module.scss";
+
+const Login = () => {
+    const suffixIcons = [
+        {
+            icon: <Emoji />,
+            onPress: () => {
+                console.log("Emoji");
+            },
+        },
+        {
+            icon: <Photo />,
+            onPress: () => {
+                console.log("Photo");
+            },
+        },
+    ];
+
+    return (
+        <div className={styles.login}>
+            <Flex h="70%" minW="800px" w="80%" px="10">
+                <Box flex={1} bg="white">
+                    <Text>Welcome to</Text>
+                    <Text color="red.700" fontSize="5xl" fontWeight="bold">
+                        A Plus
+                    </Text>
+                    <InputField
+                        placeholder="User name"
+                        variant={Variant.outline}
+                        prefixIcons={[<People />]}
+                    />
+                    <InputField
+                        placeholder="Password"
+                        variant={Variant.outline}
+                        prefixIcons={[<People />]}
+                    />
+                    <CustomButton />
+                </Box>
+                <Box flex={1} bg="red.700"></Box>
+            </Flex>
+        </div>
+    );
+};
+
+export default Login;
