372954acb9db184c0b676f271c71de4181548652
Dao Vinh Linh <linh142000@gmail.com>
2022-05-12 10:38:59 +0700

    Fix button style

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-const CustomButton = () => {
-    return <Button colorScheme="blue">Button</Button>;
+interface ICustomButton {
+    width?: string;
+    colorScheme?: string;
+    color?: string
+    text: string;
+}
+
+const CustomButton = (props: ICustomButton) => {
+    const { width, colorScheme = "red", color = "white", text } = props
+
+    return <Button w={width} colorScheme={colorScheme} borderRadius={100} color={color}>{text}</Button>;
 };
 
 export default CustomButton;
