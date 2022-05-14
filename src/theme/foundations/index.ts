74d9483145e69c06c7492ec8dcd5f115745d5419
Dao Vinh Linh <linh142000@gmail.com>
2022-05-14 14:00:20 +0700

    Add theme styles

--- src/theme/foundations/index.ts
+++ src/theme/foundations/index.ts
@@ -1,10 +1,12 @@
 import colors from './colors'
 import typography from './typography'
 import borders from './borders'
+import shadows from './shadows'
 
 const foundations = {
   colors,
   borders,
+  shadows,
   ...typography
 }
 
