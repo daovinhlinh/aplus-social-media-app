593352cd1a4c14ac1733edcd5a2b741498db2b4e
Dao Vinh Linh <linh142000@gmail.com>
2022-05-16 22:00:10 +0700

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Login from "./pages/Login";
 import NewFeed from "./pages/NewFeed";
+import NotFound from './pages/NotFound';
 
 function App() {
     return (
         <div className="App">
-            {/* <Login /> */}
-            <NewFeed />
+            <Routes >
+                <Route path='/login' element={<Login />} />
+                <Route path='/' element={<NewFeed />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,22 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RecoilRoot } from 'recoil';
 import theme from './theme';
+import { ContextProvider } from './utils/SocketContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </RecoilRoot>
+    <ContextProvider>
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </RecoilRoot>
+    </ContextProvider>
   </React.StrictMode>
 );
