34ceb93c68a496f83896b2ea44849c3db91aa6d3
linh <linh142000@gmail.com>
2022-06-07 07:38:33 +0700

    Fix style

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -4,15 +4,25 @@ import { Button } from "@chakra-ui/react";
 interface ICustomButton {
    width?: string;
    colorScheme?: string;
-   color?: string
+   color?: string;
    text: string;
-
+   onClick: (e: any) => void;
 }
 
 const CustomButton = (props: ICustomButton) => {
-   const { width, colorScheme = "red", color = "white", text } = props
+   const { width, colorScheme = "red", color = "white", text, onClick } = props;
 
-   return <Button w={width} colorScheme={colorScheme} borderRadius={5} color={color}>{text}</Button>;
+   return (
+      <Button
+         w={width}
+         colorScheme={colorScheme}
+         borderRadius={5}
+         color={color}
+         onClick={onClick}
+      >
+         {text}
+      </Button>
+   );
 };
 
 export default CustomButton;
