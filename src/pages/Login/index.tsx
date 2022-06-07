4e5b3c4fd74151157140ebd9d3da668dabde59d8
linh <linh142000@gmail.com>
2022-06-07 07:38:18 +0700

    Add axios

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,60 +1,103 @@
-import React from 'react'
-import { Box, Container, Divider, Flex, IconButton, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import { Link } from 'react-router-dom';
-import Facebook from '../../assets/Icons/Facebook';
-import Google from '../../assets/Icons/Google';
-import Key from '../../assets/Icons/Key';
+import React, { useState } from "react";
+import {
+   Box,
+   Container,
+   Divider,
+   Flex,
+   IconButton,
+   Spacer,
+   Text,
+   Wrap,
+   WrapItem,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import Facebook from "../../assets/Icons/Facebook";
+import Google from "../../assets/Icons/Google";
+import Key from "../../assets/Icons/Key";
 import People from "../../assets/Icons/People";
 import CustomButton from "../../components/CustomButton";
 import InputField, { Variant } from "../../components/InputField";
 import styles from "./styles.module.scss";
+import { axiosClient } from "../../utils/axiosClient";
 
 const Login = () => {
-    return (
-        <div className={styles.login}>
-            <Flex h="70%" minW="800px" w="70%" borderRadius={20} overflow="hidden" boxShadow='xl'>
-                <Box display="flex" flexDirection="column" bg="white" justifyContent="center" alignItems="center" minHeight="500" w="50%" px={5}>
-                    <Text fontSize="2xl" fontWeight="bold">Welcome to</Text>
-                    <Text color="red.500" fontSize="5xl" fontWeight="bold">
-                        A Plus
-                    </Text>
-                    <InputField
-                        width='70%'
-                        placeholder="User name"
-                        variant={Variant.outline}
-                        prefixIcons={[<People />]}
-                        mb={5}
-                        mt={10}
-                    />
-                    <InputField
-                        width='70%'
-                        placeholder="Password"
-                        variant={Variant.outline}
-                        prefixIcons={[<Key />]}
-                        mb={5}
-                    />
-                    <Link to={"/"}>
-                        <CustomButton width="70%" text="Login" />
-                    </Link>
-                    <Container mt={4} mb={8}>
-                        <Text as="span" color="gray.500" fontWeight="bold">Don't have an account? </Text>
-                        <Text as="span" color="red.500" fontWeight="bold">Sign Up</Text>
-                    </Container>
-                    <Divider color="gray.300" />
-                    <Text color="gray.500" fontWeight="bold" mt={4}>Or sign in with</Text>
-                    <Wrap spacing="30px" align="center" mt="5">
-                        <WrapItem>
-                            <IconButton variant='ghost' icon={<Facebook />} aria-label='Facebook' colorScheme="facebook" isRound />
-                        </WrapItem>
-                        <WrapItem>
-                            <IconButton variant='ghost' icon={<Google />} aria-label='Google' colorScheme="facebook" isRound />
-                        </WrapItem>
-                    </Wrap>
-                </Box>
-                <Box bg="red.500" w="50%"></Box>
-            </Flex>
-        </div >
-    );
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   const handleSubmit = async () => {
+      const submitForm = {
+         email,
+         password,
+      };
+
+      const res = await axiosClient.post("auth/login", submitForm);
+      console.log(res);
+
+      //   const data = await res.json();
+      //   console.log(data);
+   };
+   return (
+      <div className={styles.login}>
+         <Flex
+            h="70%"
+            minW="800px"
+            w="70%"
+            borderRadius={20}
+            overflow="hidden"
+            boxShadow="xl"
+         >
+            <Box
+               display="flex"
+               flexDirection="column"
+               bg="white"
+               justifyContent="center"
+               alignItems="center"
+               minHeight="500"
+               w="50%"
+               px={5}
+            >
+               <Text fontSize="2xl" fontWeight="bold">
+                  Welcome to
+               </Text>
+               <Text color="red.500" fontSize="5xl" fontWeight="bold">
+                  A Plus
+               </Text>
+               <InputField
+                  width="70%"
+                  placeholder="Email"
+                  variant={Variant.outline}
+                  prefixIcons={[<People />]}
+                  onChange={setEmail}
+                  mb={3}
+                  mt={10}
+               />
+               <InputField
+                  width="70%"
+                  placeholder="Password"
+                  variant={Variant.outline}
+                  prefixIcons={[<Key />]}
+                  onChange={setPassword}
+                  mb={3}
+                  type="password"
+               />
+
+               <CustomButton text="Login" onClick={handleSubmit} />
+
+               <Container mt={4} mb={8}>
+                  <Text as="span" color="gray.500" fontWeight="bold">
+                     Don't have an account?{" "}
+                  </Text>
+                  <Link to={"/register"}>
+                     <Text as="span" color="red.500" fontWeight="bold">
+                        Sign Up
+                     </Text>
+                  </Link>
+               </Container>
+            </Box>
+            <Box bg="red.500" w="50%"></Box>
+         </Flex>
+      </div>
+   );
 };
 
 export default Login;
