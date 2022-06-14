36a1b7a6c753b59a9d544ae65ae1342269a965e9
Dao Vinh Linh <linh142000@gmail.com>
2022-06-14 15:15:34 +0700

    Fix feed display issue

--- src/pages/NewFeed/index.tsx
+++ src/pages/NewFeed/index.tsx
@@ -69,7 +69,7 @@ const NewFeed = () => {
                   suffixIcons={[
                      {
                         icon: <Photo />,
-                        onPress: () => {},
+                        onPress: () => { },
                      },
                      {
                         icon: <Send />,
@@ -85,13 +85,13 @@ const NewFeed = () => {
             </Box>
             <Box>
                {fetchFeed.map((feed) => {
-                  const { _id, userId, desc, likes, createAt } = feed;
+                  const { _id, userId, desc, likes, createdAt } = feed;
                   return (
                      <FeedCard
                         desc={desc}
                         id={_id}
                         like={likes}
-                        createAt={createAt}
+                        createdAt={createdAt}
                         userId={userId}
                      />
                   );
