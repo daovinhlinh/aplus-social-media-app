d6b2b1d07849c8b72e328ebf99c7e139ebdcbddb
linh <linh142000@gmail.com>
2022-06-14 08:22:00 +0700

    Add logout

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,38 +1,55 @@
-import { Box, Center, HStack, Image } from "@chakra-ui/react";
+import { Box, Button, Center, HStack, Image } from "@chakra-ui/react";
 import React from "react";
 import Logout from "../../assets/Icons/Logout";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import Notification from "../../assets/Icons/Notification";
 import Setting from "../../assets/Icons/Setting";
 import SearchBar from "../SearchBar";
 
 const Navbar = () => {
-    const navigate = useNavigate();
+   const navigate = useNavigate();
 
-    return (
-        <HStack width="100%" justifyContent="space-between" alignItems="center" bgColor="#fff" height="8vh" py="10px" px="20px">
-            <Box
-                height={66}
-                width={92}
-                onClick={() => navigate('/')}
+   const logout = () => {
+      localStorage.clear();
+      navigate("/login");
+   };
+
+   return (
+      <HStack
+         width="100%"
+         justifyContent="space-between"
+         alignItems="center"
+         bgColor="#fff"
+         height="8vh"
+         py="10px"
+         px="20px"
+      >
+         <Box height={66} width={92} onClick={() => navigate("/")}>
+            <Image
+               src="https://i.imgur.com/udioN9R.png"
+               width="100%"
+               borderRadius="50%"
+               marginLeft={8}
+            />
+         </Box>
+         <Center minWidth="30%">
+            <SearchBar />
+         </Center>
+         <HStack spacing="30px">
+            <Setting />
+            <Notification />
+            <Button
+               bg="transparent"
+               variant="unstyled"
+               _expanded={{ boxShadow: "none" }}
+               _focus={{ boxShadow: "none" }}
+               onClick={logout}
             >
-                <Image
-                    src="https://i.imgur.com/udioN9R.png"
-                    width='100%'
-                    borderRadius='50%'
-                    marginLeft={8}
-                />
-            </Box>
-            <Center minWidth="30%">
-                <SearchBar />
-            </Center>
-            <HStack spacing="30px">
-                <Setting />
-                <Notification />
-                <Logout />
-            </HStack>
-        </HStack>
-    );
+               <Logout />
+            </Button>
+         </HStack>
+      </HStack>
+   );
 };
 
 export default Navbar;
