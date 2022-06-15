37a08370a85899752dac2dadbb17149d4730326c
linh <linh142000@gmail.com>
2022-06-15 20:28:23 +0700

    Fix fetching feed

--- src/pages/NewFeed/index.tsx
+++ src/pages/NewFeed/index.tsx
@@ -7,15 +7,14 @@ import Send from "../../assets/Icons/Send";
 import FeedCard, { IFeed } from "../../components/FeedCard";
 import InputField from "../../components/InputField";
 import PageContainer from "../../components/PageContainer";
-import { getFeed } from "../../store/feed";
 import { userDataState } from "../../store/user";
 import { axiosClient } from "../../utils/axiosClient";
 
 const NewFeed = () => {
-   const [posts, setPosts] = useState([]);
+   const [feedList, setFeedList] = useState([]);
    const [postText, setPostText] = useState("");
+   const [loading, setLoading] = useState(false);
    const userData = useRecoilValue(userDataState);
-   const feedList = useRecoilValue<IFeed[]>(getFeed);
    const [img, setImg] = useState(null);
 
    const handlePost = async () => {
@@ -34,75 +33,85 @@ const NewFeed = () => {
          await axiosClient.post("upload", form);
       }
 
-      if (postText) { 
+      if (postText) {
          await axiosClient.post("post", submitPost);
       }
    };
 
-   // useEffect(() => {
-   //    (async () => {
-   //       const fetchPosts = await axiosClient.get(
-   //          `post/timeline/${userData._id}`
-   //       );
-   //       setPosts(fetchPosts.data);
-   //    })();
-   // }, []);
+   useEffect(() => {
+      (async () => {
+         if (userData) {
+            setLoading(true);
+            const getFeed = await axiosClient.get(
+               `post/timeline/${userData._id}`
+            );
+            setFeedList(getFeed.data);
+            setLoading(false);
+         }
+      })();
+   }, [userData]);
 
-   // useEffect(() => {
-   //    console.log(feedData);
-   // }, [feedData]);
- 
+   if (loading) {
+      return <div>Loading...</div>;
+   }
+
    return (
       <React.Suspense fallback={<div>Loading...</div>}>
          <PageContainer page="Post">
-          {
-             feedList.length > 0 &&
-            <>
-            <Box
-               width={"100%"}
-               bgColor="#FFF"
-               px="30px"
-               py="20px"
-               borderRadius="15px"
-               mb="40px"
-            >
-               <Text textAlign="left" color="black" fontWeight={600} mb="29px">
-                  Post something
-               </Text>
-               <InputField
-                  suffixIcons={[
-                     {
-                        icon: <Photo />,
-                        onPress: () => { },
-                     },
-                     {
-                        icon: <Send />,
-                        onPress: handlePost,
-                     },
-                  ]}
-                  onChange={(text) => {
-                     setPostText(text);
-                  }}
-                  image="https://www.w3schools.com/howto/img_avatar.png"
-                  placeholder="Post something..."
-               />
-            </Box>
-            <Box>
-               {feedList.map((feed: IFeed) => {
-                  const { _id, userId, desc, likes, createdAt } = feed;
-                  return (
-                     <FeedCard
-                        desc={desc}
-                        _id={_id}
-                        likes={likes}
-                        createdAt={createdAt}
-                        userId={userId}
+            {feedList.length > 0 && (
+               <>
+                  <Box
+                     width={"100%"}
+                     bgColor="#FFF"
+                     px="30px"
+                     py="20px"
+                     borderRadius="15px"
+                     mb="40px"
+                  >
+                     <Text
+                        textAlign="left"
+                        color="black"
+                        fontWeight={600}
+                        mb="29px"
+                     >
+                        Post something
+                     </Text>
+                     <InputField
+                        suffixIcons={[
+                           {
+                              icon: <Photo />,
+                              onPress: () => {},
+                           },
+                           {
+                              icon: <Send />,
+                              onPress: handlePost,
+                           },
+                        ]}
+                        onChange={(text) => {
+                           setPostText(text);
+                        }}
+                        image="https://www.w3schools.com/howto/img_avatar.png"
+                        placeholder="Post something..."
                      />
-                  );
-               }).reverse()}
-            </Box>
-            </>
-            }
+                  </Box>
+                  <Box>
+                     {feedList
+                        .map((feed: IFeed) => {
+                           const { _id, userId, desc, likes, createdAt } = feed;
+                           return (
+                              <FeedCard
+                                 desc={desc}
+                                 _id={_id}
+                                 likes={likes}
+                                 createdAt={createdAt}
+                                 userId={userId}
+                              />
+                           );
+                        })
+                        .reverse()}
+                  </Box>
+               </>
+            )}
          </PageContainer>
       </React.Suspense>
    );
--- src/store/feed.ts
+++ src/store/feed.ts
@@ -6,13 +6,3 @@ const FeedState = atom({
    key: "feed",
    default: [],
 });
-
-export const getFeed = selector({
-   key: "getFeed",
-   get: async ({ get }: any) => {
-      const fetchPosts = await axiosClient.get(
-         `post/timeline/629ce3b070ad0da4575387b8`
-      );
-      return fetchPosts.data;
-   },
-});
