80a7bb6c361dff536ec04709d7e9b137cff1d93c
tungdz202 <anhthu28102000@gmail.com>
2022-06-24 21:43:06 +0700

    Remove unuse component

--- src/pages/Group/Profile/index.tsx
+++ src/pages/Group/Profile/index.tsx
@@ -11,51 +11,47 @@ import Photo from "../../../assets/Icons/Photo";
 import Video from "../../../assets/Icons/Video";
 
 const ProfileGroup = () => {
-   const featureList = [
-      {
-         title: "Profile",
-         icon: <IconProfile />,
-         link: "/group",
-      },
-      {
-         title: "Post",
-         icon: <Post />,
-         link: "/postG",
-      },
-      {
-         title: "Members",
-         icon: <Friends />,
-         link: "/friendG",
-      },
-      {
-         title: "Photos",
-         icon: <Photo />,
-         link: "/photo",
-      },
-      {
-         title: "Videos",
-         icon: <Video />,
-         link: "/video",
-      },
-   ];
-   return (
-      <div className={styles.profile} >
+  const featureList = [
+    {
+      title: "Profile",
+      icon: <IconProfile />,
+      link: "/group",
+    },
+    {
+      title: "Post",
+      icon: <Post />,
+      link: "/postG",
+    },
+    {
+      title: "Members",
+      icon: <Friends />,
+      link: "/friendG",
+    },
+    {
+      title: "Photos",
+      icon: <Photo />,
+      link: "/photo",
+    },
+    {
+      title: "Videos",
+      icon: <Video />,
+      link: "/video",
+    },
+  ];
+  return (
+    <div className={styles.profile}>
       <PageContainer isAdmin={true} featureList={featureList}>
-         
-            <Box>
-               <div className={styles.backgroundGprofile}>
-                  <BackgroundProfile />
-               </div>
-            </Box>
-            <Box>
-               <div className={styles.mainprofileG}>
-                  <MainProfileG />
-               </div>
-            </Box>
-         
+        <Box>
+          <div className={styles.backgroundGprofile}></div>
+        </Box>
+        <Box>
+          <div className={styles.mainprofileG}>
+            <MainProfileG />
+          </div>
+        </Box>
       </PageContainer>
-      </div>
-   );
+    </div>
+  );
 };
 
 export default ProfileGroup;
