3d1eb382869129ba188964ad72003293ec17e2ff
tungdz202 <anhthu28102000@gmail.com>
2022-06-24 21:32:51 +0700

    Fetch data

--- src/components/FriendsList/index.tsx
+++ src/components/FriendsList/index.tsx
@@ -1,37 +1,25 @@
 import { Box, Image, SimpleGrid, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 import Options from "../../assets/Icons/Options";
+import { userDataState } from "../../store/user";
+import { axiosClient } from "../../utils/axiosClient";
 import DetailCard from "../DetailCard";
 
-const detailcards = [
-   {
-      title1: "Dao vinh ling",
-      title2: "buck foiz",
-      img: "https://scontent.fhan11-1.fna.fbcdn.net/v/t31.18172-8/26841225_744862592376172_998804515399431440_o.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=Ckvxss-PJRoAX9aghkw&_nc_ht=scontent.fhan11-1.fna&oh=00_AT_Ee3hxiRkntw7aXCgRvyBBrdmPOLxrOuh-n-9gSm35Hw&oe=62A82296",
-   },
-   {
-      title1: "Bui Tung Anh",
-      title2: "Thơ nụ",
-      img: "https://scontent.fhan11-1.fna.fbcdn.net/v/t31.18172-8/26841225_744862592376172_998804515399431440_o.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=Ckvxss-PJRoAX9aghkw&_nc_ht=scontent.fhan11-1.fna&oh=00_AT_Ee3hxiRkntw7aXCgRvyBBrdmPOLxrOuh-n-9gSm35Hw&oe=62A82296",
-   },
-   {
-      title1: "Đỗ Thái Sơn",
-      title2: "Kiểm Lâm",
-      img: "https://scontent.fhan11-1.fna.fbcdn.net/v/t31.18172-8/26841225_744862592376172_998804515399431440_o.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=Ckvxss-PJRoAX9aghkw&_nc_ht=scontent.fhan11-1.fna&oh=00_AT_Ee3hxiRkntw7aXCgRvyBBrdmPOLxrOuh-n-9gSm35Hw&oe=62A82296",
-   },
-   {
-      title1: "Hồng Anh Amie",
-      title2: "Thủy Quân Lục Túi",
-      img: "https://scontent.fhan11-1.fna.fbcdn.net/v/t31.18172-8/26841225_744862592376172_998804515399431440_o.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=Ckvxss-PJRoAX9aghkw&_nc_ht=scontent.fhan11-1.fna&oh=00_AT_Ee3hxiRkntw7aXCgRvyBBrdmPOLxrOuh-n-9gSm35Hw&oe=62A82296",
-   },
-   {
-      title1: "Đỗ Thái Sơn",
-      title2: "Kiểm Lâm",
-      img: "https://scontent.fhan11-1.fna.fbcdn.net/v/t31.18172-8/26841225_744862592376172_998804515399431440_o.jpg?_nc_cat=105&ccb=1-6&_nc_sid=174925&_nc_ohc=Ckvxss-PJRoAX9aghkw&_nc_ht=scontent.fhan11-1.fna&oh=00_AT_Ee3hxiRkntw7aXCgRvyBBrdmPOLxrOuh-n-9gSm35Hw&oe=62A82296",
-   },
-];
-
 const Friendslist = (props: any) => {
+   const userData = useRecoilValue(userDataState);
+   const [friends, setFriends] = useState([]);
+   const imgFolder = import.meta.env.VITE_CDN_URL;
+
+   useEffect(() => {
+      getFriendList();
+   }, []);
+
+   const getFriendList = async () => {
+      const getFriendRes = await axiosClient.get(`/user/friends/${userData._id}`);
+      setFriends(getFriendRes.data);
+   };
+
    return (
       <Box
          borderRadius={20}
@@ -46,24 +34,19 @@ const Friendslist = (props: any) => {
             Friends
          </Text>
          <SimpleGrid columns={2} minChildWidth="45%" spacing="20px">
-            {detailcards.map((item) => (
+            {friends.map((item) => (
                <DetailCard
-                  label2={item.title2}
-                  label1={item.title1}
-                  leftImg={
-                     <Image
-                        src={item.img}
-                        h="70px"
-                        w="70px"
-                        borderRadius="10px"
-                     />
-                  }
-                  rightIcon={<Options />}
-                  onClick={() => {}}
-                  width="100%"
+                  key={item._id}
+                  label2={item.username}
+                  imgSize={35}
+                  leftImg={`${imgFolder}${item.avatar}`}
+                  // rightIcon={<Options />}
+                  onClick={() => { }}
+                  width={friends.length > 1 ? "100%" : "50%"}
                   border="1px solid #E5E5E5"
                   borderRadius={15}
                   alignItems="center"
+                  padding="5px"
                />
             ))}
          </SimpleGrid>
